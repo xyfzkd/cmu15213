8bae0cb71a9474b1f03430595551d5107e4f5c33
xyfzkd <xyfzkd@outlook.com>
2020-07-18 00:06:50 +0800

    debug_ex

--- 6_MallocLab/malloclab-handout/README.md
+++ 6_MallocLab/malloclab-handout/README.md
@@ -54,6 +54,133 @@ so add bp label to place, extend_heap, and free function
 at first I add split and first option to distinguish the situation.fi when I do the formmer thing, this can be omitted.
 
 
+### debug
+free_list bug
+how to find it?
+
+* `r -f traces/am*.rep` when alloc at 694 times, corrupt!
+
+```
+692 times: from address 0xf699e020 to 0xf6af4680, check contiguous free block:
+No escaped coalescing, congras! There are 420 alloc blocks and 25 free blocks.
+693 times: from address 0xf699e020 to 0xf6af4680, check contiguous free block:
+No escaped coalescing, congras! There are 421 alloc blocks and 25 free blocks.
+``` 
+* write `free_block_coont` and `free_list_count` function, set bp at `mm_malloc` and call them in `gdb`. Both of them return 1; when `c 694`, only `free_list_count` returns.
+
+```
+(gdb) call check_list()
+0xf6a540e8      0xf6a57258      0xf6a857e0      80
+0xf6a57258      0xf6a5d058      0xf6a540e8      3664
+0xf6a5d058      0xb5b5b5b5      0xb5b5b5b5      4080
+
+Program received signal SIGSEGV, Segmentation fault.
+
+```
+check malloc trace in .rep,
+
+```
+a 686 160
+f 685
+a 687 4072
+a 688 4072
+a 689 4072
+a 690 4072
+a 691 4072
+a 692 4072
+a 693 4072
+a 694 4072
+a 695 4072
+```
+a lot of alloc, maybe placing uncorrectly leads to the issue
+
+```
+c 690
+(gdb) call check_list()
+0xf6aac0e8      0xf6aaf258      0xf6add7e0      80
+0xf6aaf258      0xf6ab2088      0xf6aac0e8      3664
+0xf6ab2088      0xf6ab5058      0xf6aaf258      40880
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+0xf6ab5058      0xf6ab3078      0xf6ab2088      28640
+0xf6ab3078      0xf6ab5058      0xf6ab2088      36800
+```
+one princple: be wrong at first, or be right
+so check mm_free
+
+```
+break mm_free
+r -f traces/amptjp-bal.rep
+call check_list()
+c
+call check_list()
+c
+call check_list()
+c
+...
+```
+
+```
+343 times: from address 0xf6964020 to 0xf69fea20, check contiguous free block:
+No escaped coalescing, congras! There are 342 alloc blocks and 1 free blocks.
+344 times: from address 0xf6964020 to 0xf69feac8, check contiguous free block:
+No escaped coalescing, congras! There are 343 alloc blocks and 1 free blocks.
+
+Breakpoint 1, mm_free (ptr=0xf69feab8) at mm.c:336
+336     {
+0xf69feb38      0xf69feb38      0xf69feb38      16
+
+Breakpoint 1, mm_free (ptr=0xf69fe9c0) at mm.c:336
+336     {
+0xf69feab8      0xf69feab8      0xf69feab8      144
+345 times: from address 0xf6964020 to 0xf69feb48, check contiguous free block:
+No escaped coalescing, congras! There are 342 alloc blocks and 2 free blocks.
+346 times: from address 0xf6964020 to 0xf69ffb38, check contiguous free block:
+No escaped coalescing, congras! There are 343 alloc blocks and 2 free blocks.
+347 times: from address 0xf6964020 to 0xf6a00b28, check contiguous free block:
+No escaped coalescing, congras! There are 344 alloc blocks and 2 free blocks.
+348 times: from address 0xf6964020 to 0xf6a00b28, check contiguous free block:
+No escaped coalescing, congras! There are 345 alloc blocks and 2 free blocks.
+
+Breakpoint 1, mm_free (ptr=0xf6a00a98) at mm.c:336
+336     {
+0xf6a00b90      0xf6a00b90      0xf6a00b90      64
+0xf6a00b90      0xf6a00b90      0xf6a00b90      64
+349 times: from address 0xf6964020 to 0xf6a00bd0, check contiguous free block:
+No escaped coalescing, congras! There are 345 alloc blocks and 3 free blocks.
+
+```
+```
+a 343 160
+a 344 120
+f 344
+f 342
+a 345 4072
+a 346 4072
+a 347 72
+a 348 160
+f 347
+a 349 72
+```
+
 ## Results
 first fit
 
--- 6_MallocLab/malloclab-handout/mm.c
+++ 6_MallocLab/malloclab-handout/mm.c
@@ -1,5 +1,9 @@
+//
+// Created by 谢宇锋 on 2020/7/14.
+//
+
 /*
- mm.c Implicit List links all blocks
+ mm.c Explicit List links free blocks
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,22 +19,22 @@
  * provide your team information in the following struct.
  ********************************************************/
 team_t team = {
-    /* Team name */
-    "malloclab",
-    /* First member's full name */
-    "Yufeng Xie",
-    /* First member's email address */
-    "xyf19@mails.tsinghua.edu.cn",
-    /* Second member's full name (leave blank if none) */
-    "",
-    /* Second member's email address (leave blank if none) */
-    ""
+        /* Team name */
+        "malloclab",
+        /* First member's full name */
+        "Yufeng Xie",
+        /* First member's email address */
+        "xyf19@mails.tsinghua.edu.cn",
+        /* Second member's full name (leave blank if none) */
+        "",
+        /* Second member's email address (leave blank if none) */
+        ""
 };
 
 /* basic constants and macros */
 #define WSIZE 4 /* word_t size in header or footer area */
 #define DSIZE 8
-#define CHUNKSIZE (1<<3)
+#define CHUNKSIZE (1<<4)
 
 /* rounds up to the nearest multiple of DSIZE */
 #define align(size) (((size) + (DSIZE-1)) & ~0x7)
@@ -47,31 +51,26 @@ team_t team = {
 
 #define max(x, y) ((x)>(y)?(x):(y))
 
+#define f_next_set(bp,val) (put(bp,(unsigned int)(val)))
+
+#define f_prev_set(bp,val) (put(((unsigned int *)(bp)+1),(unsigned int)(val)))
+
+#define f_next(bp) ((unsigned int *)*(unsigned int *)(bp))
+
+#define f_prev(bp) ((unsigned int *)*((unsigned int *)(bp)+1))
+
 /* bp point to the payload of the block, h_bp calculate the header address
  * while f_bp calculate the footer address
- * and there are next_block and prev_block to locate the address of 
+ * and there are next_block and prev_block to locate the address of
  * corresponding block
  */
 #define h_bp(bp) ((char *)(bp) - WSIZE)
 #define f_bp(bp) ((char *)(bp) + get_size(h_bp(bp))-DSIZE)
 #define next_block(bp) ((char *)(bp) + get_size(((char *)(bp)-WSIZE)))
+/* in the situation of No Boundary Tag, prev_block when the previous block is free*/
 #define prev_block(bp) ((char *)(bp) - get_size(((char *)(bp)-DSIZE)))
 
 static char* heap_lists;
-static char* heap_liste;
-
-/******************************************************************
- * heap consistency checker
- * Is every block in the free list marked as free?
- * Are there any contiguous free blocks that somehow escaped coalescing?
- * Is every free block actually in the free list?
- * Do the pointers in the free list point to valid free blocks?
- * Do any allocated blocks overlap?
- * Do the pointers in a heap block point to valid heap addresses?
- ******************************************************************/
-
-void *escaped_coalescing();
-
 
 /******************************************************************
  * Implicit List: Function prototypes
@@ -85,33 +84,31 @@ void *mm_malloc(size_t size);
 void mm_free(void *ptr);
 void *mm_realloc(void *ptr, size_t size);
 
+unsigned int *free_list_bp;
+
+int init = 1;
+
+
+
+void *escaped_coalescing();
 
 
 /******************************************************************
- * Implicit List: Routines
+ * Explicit List: Routines
  *****************************************************************/
 
-// first fit
 void *find_fit(size_t asize){
-    char *bp=(char*) mem_heap_lo()+4*WSIZE,*hbp;
-    for(; bp!=(char*) mem_heap_hi()+1; bp = next_block(bp)){
+    char *hbp;
+    unsigned int *bp = free_list_bp;
+    if(bp==NULL) return NULL;
+    do{
         hbp = h_bp(bp);
         if (!(get_alloc(hbp)) && (asize<=get_size(hbp)))
             return bp;
-    }
+        bp = f_next(bp);
+    }while (bp!=free_list_bp);
     return NULL;
 }
-// next fit
-int first = 1, split=0;
-char *next_bp;
-void *next_fit(size_t asize){
-    char *hbp = h_bp(next_bp);
-    if (!(get_alloc(hbp)) && (asize<=get_size(hbp))){
-        return next_bp;
-    }
-    return find_fit(asize);
-}
-
 
 static void *extend_heap(size_t words){
     char *bp;
@@ -122,9 +119,25 @@ static void *extend_heap(size_t words){
     put(h_bp(bp),pack(size,0));
     put(f_bp(bp),pack(size,0));
     put(h_bp(next_block(bp)),pack(0,1));
-    return next_bp=coalesce(bp);
+    /* case 1 and case 3
+     * case 1
+     * E2=E1=S1=S2       E2=E1 S3=S1=S2
+     *        |                 |
+     *       root              root
+     */
+    if(free_list_bp==NULL){
+        free_list_bp = (unsigned int *)bp;
+        f_next_set(bp,bp);
+        f_prev_set(bp,bp);
+        return bp;
+    }
+    unsigned int *free_list_bp_old = free_list_bp;
+    bp = coalesce(bp);
+//    f_prev_set(bp,f_prev(free_list_bp_old));
+//    f_next_set(f_prev(free_list_bp_old),bp);
+    return bp;
 }
-/* 
+/*
  * mm_init - initialize the malloc package.
  */
 
@@ -135,33 +148,83 @@ int mm_init(void)
     put(heap_lists,0);
     put(heap_lists+(1*WSIZE),pack(DSIZE,1));
     put(heap_lists+(2*WSIZE),pack(DSIZE,1));
-    heap_liste = heap_lists+(3*WSIZE);
-    put(heap_liste,pack(0,1));
+    put(heap_lists+(3*WSIZE),pack(0,1));
     heap_lists += 2*WSIZE;
-    if (extend_heap(CHUNKSIZE/WSIZE)==NULL)
+    size_t size = align(CHUNKSIZE);
+    if ((free_list_bp = mem_sbrk(size))==(void *)-1)
         return -1;
+    put(h_bp(free_list_bp),pack(size,0));
+    put(f_bp(free_list_bp),pack(size,0));
+    put(h_bp(next_block(free_list_bp)),pack(0,1));
+    f_next_set(free_list_bp,free_list_bp);
+    f_prev_set(free_list_bp,free_list_bp);
+    //f_prev_set(heap_lists+2*WSIZE,free_list_bp);
     return 0;
 }
 
-/* 
+/*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void place(char *bp, size_t asize){
     size_t fit_size = get_size(h_bp(bp));
-    if((fit_size - asize)>= 2*DSIZE){
-        put(h_bp(bp), pack(asize,1));
-        put(f_bp(bp), pack(asize,1));
-        char *bpn = next_block(bp);
-        put(h_bp(bpn), pack(fit_size-asize,0));
-        put(f_bp(bpn), pack(fit_size-asize,0));
-        //next fit
-        split = 1;
-        next_bp = bpn;
-    } else{
-        put(h_bp(bp), pack(fit_size,1));
-        put(f_bp(bp), pack(fit_size,1));
-        split = 0;
+    if((bp!=(char*)free_list_bp)) {
+        if ((fit_size - asize) >= 2 * DSIZE) {
+            put(h_bp(bp), pack(asize, 1));
+            put(f_bp(bp), pack(asize, 1));
+            char *bpn = next_block(bp);
+            put(h_bp(bpn), pack(fit_size - asize, 0));
+            put(f_bp(bpn), pack(fit_size - asize, 0));
+            /* free remainder considered*/
+            f_next_set(bpn, f_next(bp));
+            f_prev_set(bpn, f_prev(bp));
+            f_next_set(f_prev(bp), bpn);
+            f_prev_set(f_next(bp), bpn);
+        } else {
+            put(h_bp(bp), pack(fit_size, 1));
+            put(f_bp(bp), pack(fit_size, 1));
+
+            f_next_set(f_prev(bp), f_next(bp));
+            f_prev_set(f_next(bp), f_prev(bp));
+        }
+    }else if(f_next(bp)==(unsigned int *)bp){
+        if ((fit_size - asize) >= 2 * DSIZE) {
+            put(h_bp(bp), pack(asize, 1));
+            put(f_bp(bp), pack(asize, 1));
+            char *bpn = next_block(bp);
+            put(h_bp(bpn), pack(fit_size - asize, 0));
+            put(f_bp(bpn), pack(fit_size - asize, 0));
+            free_list_bp = (unsigned int *) bpn;
+            f_next_set(bpn,bpn);
+            f_prev_set(bpn,bpn);
+        }
+        else{
+            put(h_bp(bp), pack(fit_size, 1));
+            put(f_bp(bp), pack(fit_size, 1));
+            free_list_bp = NULL;
+        }
+    }else{
+        if ((fit_size - asize) >= 2 * DSIZE) {
+            put(h_bp(bp), pack(asize, 1));
+            put(f_bp(bp), pack(asize, 1));
+            char *bpn = next_block(bp);
+            put(h_bp(bpn), pack(fit_size - asize, 0));
+            put(f_bp(bpn), pack(fit_size - asize, 0));
+            free_list_bp = (unsigned int *) bpn;
+            /* free remainder considered*/
+            f_next_set(bpn, f_next(bp));
+            f_prev_set(bpn, f_prev(bp));
+            f_next_set(f_prev(bp), bpn);
+            f_prev_set(f_next(bp), bpn);
+        }
+        else{
+            put(h_bp(bp), pack(fit_size, 1));
+            put(f_bp(bp), pack(fit_size, 1));
+
+            f_next_set(f_prev(bp), f_next(bp));
+            f_prev_set(f_next(bp), f_prev(bp));
+            free_list_bp = f_next(bp);
+        }
     }
 }
 void *mm_malloc(size_t size)
@@ -169,11 +232,11 @@ void *mm_malloc(size_t size)
     size_t asize;
     size_t extendsize;
     char *bp;
-    //escaped_coalescing();
+    escaped_coalescing();
     if(size == 0) return NULL;
     if (size <= DSIZE) asize = 2*DSIZE;
-    else asize = align(size) + DSIZE;
-    if ((bp = next_fit(asize))!=NULL)
+    else asize = align(size)+DSIZE;
+    if ((bp = find_fit(asize))!=NULL)
     {
         place(bp, asize);
         return bp;
@@ -192,26 +255,81 @@ static void *coalesce(void *bp){
     size_t next_alloc = get_alloc(h_bp(next_block(bp)));
     size_t size = get_size(h_bp(bp));
 
-    if (prev_alloc && next_alloc) return bp;
+    if (prev_alloc && next_alloc) {
+        if(free_list_bp!=NULL){
+            f_next_set(f_prev(free_list_bp),bp);
+            f_prev_set(bp,f_prev(free_list_bp));
+            f_next_set(bp,free_list_bp);
+            f_prev_set(free_list_bp,bp);
+
+            /* root point to bp */
+            free_list_bp = bp;
+        } else{
+            free_list_bp = bp;
+            f_next_set(bp,free_list_bp);
+            f_prev_set(free_list_bp,bp);
+        }
+
+        return bp;
+    }
     else if (prev_alloc && !next_alloc){
+        void *nf = next_block(bp);
         size += get_size(h_bp(next_block(bp)));
         put(h_bp(bp),pack(size,0));
         put(f_bp(bp),pack(size,0));
+
+        f_next_set(f_prev(free_list_bp),bp);
+        f_prev_set(bp,f_prev(free_list_bp));
+
+        f_next_set(bp,free_list_bp);
+        f_prev_set(free_list_bp,bp);
+
+        free_list_bp = bp;
+        f_next_set(f_prev(nf),f_next(nf));
+        f_prev_set(f_next(nf),f_prev(nf));
     }
     else if (!prev_alloc && next_alloc){
+        void *pf = prev_block(bp);
         size += get_size(f_bp(prev_block(bp)));
         put(h_bp(prev_block(bp)),pack(size,0));
         put(f_bp(prev_block(bp)),pack(size,0));
         bp = prev_block(bp);
+        if(bp!=free_list_bp){
+            f_prev_set(f_next(pf),f_prev(pf));
+            f_next_set(f_prev(pf),f_next(pf));
+
+            f_next_set(f_prev(free_list_bp),bp);
+            f_prev_set(bp,f_prev(free_list_bp));
+
+            f_next_set(pf,free_list_bp);
+            f_prev_set(free_list_bp,pf);
+        }
+        free_list_bp = bp;
+
+
     }
     else{
+        void *pf = prev_block(bp);
+        void *nf = next_block(bp);
         size += get_size(f_bp(prev_block(bp)))+get_size(h_bp(next_block(bp)));
         put(h_bp(prev_block(bp)),pack(size,0));
         put(f_bp(prev_block(bp)),pack(size,0));
         bp = prev_block(bp);
+
+        f_next_set(f_prev(free_list_bp),bp);
+        f_prev_set(bp,f_prev(free_list_bp));
+
+        f_next_set(pf,free_list_bp);
+        f_prev_set(free_list_bp,pf);
+        free_list_bp = pf;
+        f_prev_set(f_next(pf),f_prev(pf));
+        f_next_set(f_prev(pf),f_next(pf));
+        f_next_set(f_prev(nf),f_next(nf));
+        f_prev_set(f_next(nf),f_prev(nf));
     }
     return bp;
 }
+
 
 /*
  * mm_free - Freeing a block does nothing.
@@ -221,7 +339,7 @@ void mm_free(void *ptr)
     size_t size = get_size(h_bp(ptr));
     put(h_bp(ptr),pack(size,0));
     put(f_bp(ptr),pack(size,0));
-    next_bp = coalesce(ptr);
+    coalesce(ptr);
 }
 
 /*
@@ -238,30 +356,27 @@ void *mm_realloc(void *ptr, size_t size)
     size_t copySize;
     newptr = mm_malloc(size);
     if (newptr == NULL)
-      return NULL;
+        return NULL;
     if(oldptr == NULL)
         return newptr;
     copySize = get_size(h_bp(oldptr))-DSIZE;
     if (size < copySize)
-      copySize = size;
+        copySize = size;
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
 }
 
-
-
-
-
 /******************************************************************
  * heap consistency checker routines
  ******************************************************************/
 void *escaped_coalescing(){
+    static int count = 0;
     char *bp = (char*)mem_heap_lo()+4*WSIZE;
     char *lastbp = (char*)mem_heap_hi()+1;
     int nfblocks = 0, nablocks=0;
     int fblock = 0;
-    printf("from address %p to %p, check contiguous free block:\n",(void*)bp,(void *)lastbp);
+    printf("%d times: from address %p to %p, check contiguous free block:\n",count,(void*)bp,(void *)lastbp);
     for(;bp!=lastbp;bp=next_block(bp)){
         if(get_alloc(h_bp(bp))==0){
             if(fblock==1){
@@ -276,13 +391,90 @@ void *escaped_coalescing(){
         }
     }
     printf("No escaped coalescing, congras! There are %d alloc blocks and %d free blocks.\n",
-            nablocks,nfblocks);
+           nablocks,nfblocks);
+    count++;
     return NULL;
 }
 
+/*
+ * Is every block in the free list marked as free?
+ */
+int mark_free(){
+    unsigned int *bp = free_list_bp;
+    do{
+        if(get_alloc(h_bp(free_list_bp)))
+            return 0;
+        bp = f_next(bp);
+    }while(bp!=free_list_bp);
+    return 1;
+}
 
+int free_list_count(){
+    unsigned int *bp = free_list_bp;
+    int counts = 0;
+    do{
+        counts++;
+        bp = f_next(bp);
+    }while (bp!=free_list_bp);
+    return counts;
+}
 
+int free_block_count(){
+    int counts = 0;
+    char *bp = (char*)mem_heap_lo()+4*WSIZE;
+    char *lastbp = (char*)mem_heap_hi()+1;
+    for(;bp!=lastbp;bp=next_block(bp)){
+        if(!get_alloc(h_bp(bp)))
+            counts++;
+    }
+    return counts;
+}
+/* print list pointer info and check list
+ * a    b   c
+ * b    d   a
+ * d    e   b
+ * check the first and second columns paired.
+ * check the first and third columns paired
+ */
+int check_list(){
+    int counts = free_block_count();
+    unsigned int * bp = free_list_bp;
+    unsigned int a0=0,a=0,b=0,c=0,c0=0;
+    for (int i=0; i<counts; i++){
+        if (i==0) {
+            a0 = a = (unsigned int)bp;
+            b = (unsigned int)f_next(bp);
+            c0 = c = (unsigned int)f_prev(bp);
+        }else {
+            c = (unsigned int)f_prev(bp);
+            if (a == c) a = (unsigned int)bp;
+            else return 0;
 
+            if (a == b) b = (unsigned int)f_next(bp);
+            else return 0;
+        }
 
-
-
+        printf("%p\t%p\t%p\t%d\n",bp,f_next(bp),f_prev(bp),get_size(h_bp(bp)));
+        bp = f_next(bp);
+    }
+    if ((b==a0)&&(c0==a)) return 1;
+    else return 0;
+}
+/*
+ * Is every free block actually in the free list?
+ */
+//int block_in_list(){
+//    char *bp = (char*)mem_heap_lo()+4*WSIZE;
+//    char *lastbp = (char*)mem_heap_hi()+1;
+//    unsigned int *bpl = free_list_bp;
+//    for(;bp!=lastbp;bp=next_block(bp)){
+//        if(!get_alloc(h_bp(bp))){
+//            do{
+//                if(bp==bpl) break;
+//                bpl = f_next(bpl);
+//            }while (bpl!=free_list_bp);
+//            return 0;
+//        }
+//    }
+//    return 1;
+//}
